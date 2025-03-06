459a6064611ef3d5a164fca21ca07766f17e8486
Tyler <tyler.whitchurch@atlasschool.com>
2025-03-06 11:17:58 -0600

    jk

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -2,9 +2,9 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
   /* eslint-disable no-const-assign, no-unused-vars */
-    if (trueOrFalse) {
-      const task = true;
-      const task2 = false;
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
     }
   
     return [task, task2];
