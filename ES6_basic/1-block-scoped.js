7efe9f317fbf5473a5b53c63cd763c38389e77a5
Tyler <tyler.whitchurch@atlasschool.com>
2025-03-06 11:00:33 -0600

    eslinnt sucks

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
     const task2 = true;
 /* eslint-disable no-const-assign, no-unused-vars */
     if (trueOrFalse) {
-      const task = true;
-      const task2 = false;
+    const task = true;
+    const task2 = false;
     }
   
     return [task, task2];
