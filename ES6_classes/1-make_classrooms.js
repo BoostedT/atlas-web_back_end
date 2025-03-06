8f23aaef837d424705644333581f49a7cb2e882c
Tyler <tyler.whitchurch@atlasschool.com>
2025-03-06 14:22:03 -0600

    removed space

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,7 +1,7 @@
-  /* eslint-disable-next-line import/extensions */
+/* eslint-disable-next-line import/extensions */
 import Classroom from './0-classroom.js';
 
-  /* eslint-disable-next-line no-multiple-empty-lines */
+/* eslint-disable-next-line no-multiple-empty-lines */
 export default function initializeRooms() {
   const rooms = [
     new Classroom(19),
