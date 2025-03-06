cd4185e98708e7b103bbe410986cb7e9170bcc7f
Tyler <tyler.whitchurch@atlasschool.com>
2025-03-06 10:47:43 -0600

    lint

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-    var task = false;
-    var task2 = true;
-
-if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    let task = false;
+    let task2 = true;
+  
+    if (trueOrFalse) {
+      task = true;
+      task2 = false;
     }
-
+  
     return [task, task2];
-}
+  }
+  
