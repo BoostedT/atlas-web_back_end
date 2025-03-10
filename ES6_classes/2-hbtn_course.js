ca7d1b30cc379fdf43dde993a42581b1a8eafa2c
BoostedT <tyler.whitchurch@atlasschool.com>
2025-03-10 13:58:23 -0500

    finished code

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -12,21 +12,44 @@ export default class HolbertonCourse {
     if (students.every((student) => typeof student !== 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
-    this.name = name
-    this.length = length
-    this.students = students
+    this._name = name
+    this._length = length
+    this._students = students
   }
 
   get name() {
     return this._name
   }
-  set name(newnName) {
-    if (typeof newnName !== 'string') {
+
+  set name(value) {
+    if (typeof value!== 'string') {
       throw new TypeError('Name must be a string');
     }
-    this._name = newnName
+    this._name = value
   }
+  
   get length() {
     return this._length
   }
-  set length(newLength) {
+
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = value
+  }
+
+  get students() {
+    return this._students
+  }
+
+  set students(value) {
+    if (!Array.isArray(value)) {
+      throw new TypeError('Students must be an array');
+    }
+    if (value.every((student) => typeof student !== 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = value
+  }
+}
