36d97a9ce1c5583e361efbb5e0bbd76b60cc3443
BoostedT <tyler.whitchurch@atlasschool.com>
2025-03-10 14:13:36 -0500

    replaced semicolon with quotes

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
 export default function getListStudents() {
   return [
-    {firstname: 'Guillamue', id: 1, location: 'San Francisco'},
-    {firstname: 'James', id: 2, location: 'Columbia'},
-    {firstname: 'Serena', id: 5, location: 'San Francisco'},
+    {firstname: "Guillamue", id: 1, location: 'San Francisco'},
+    {firstname: "James", id: 2, location: 'Columbia'},
+    {firstname: "Serena", id: 5, location: 'San Francisco'},
   ];
 }
