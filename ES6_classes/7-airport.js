cd1eecfcdefce3d3f8ceb41a0ce2f1cb631d66a7
BoostedT <tyler.whitchurch@atlasschool.com>
2025-03-10 13:13:13 -0500

    made a class called Holberton where the size must be a # and location must be a sting

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,17 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size!== 'number') throw Error('Size must be a number');
+    if (typeof location!== 'string') throw Error('Location must be a string');
+
+    this._size = size;
+    this._location = location;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+
+  toString() {
+    return this._location;
+  }
+}
