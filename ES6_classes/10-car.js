7077aaac62719c3b19f3b28862dce113ffddb864
BoostedT <tyler.whitchurch@atlasschool.com>
2025-03-10 13:54:45 -0500

    removed error

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,7 +1,5 @@
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof brand!== 'string' || typeof motor!== 'string' || typeof color!== 'string') throw Error('All attributes must be strings');
-
     this._brand = brand;
     this._motor = motor;
     this._color = color;
