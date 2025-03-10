91eb21f005d93b8f2c7b1e7810469270d17243a1
BoostedT <tyler.whitchurch@atlasschool.com>
2025-03-10 12:02:04 -0500

    i think i got it

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -2,25 +2,24 @@ export default class Currency {
   constructor(code, name) {
     this._code = code;
     this._name = name;
-    }
+}
 
   get code() {
-  return this._code;
-    }
+    return this._code;
+}
 
   set code(value) {
-  this._code = value;
-    }
+    this._code = value;
+}
 
   get name() {
-  return this._name;
-    }
+    return this._name;
+}
 
   set name(value) {
   this._name = value;
-    }
-
-    displayFullCurrency() {
-      return `${this._name} (${this._code})`;
-    }
+}
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
   }
+}
