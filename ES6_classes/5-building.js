262f868626575add5822c0b792ede4e6881f27be
BoostedT <tyler.whitchurch@atlasschool.com>
2025-03-10 12:33:32 -0500

    added updated to the building class

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,5 +1,8 @@
 export default class Building {
   constructor(sqft) {
+    if (this.constructor === Building) {
+      throw new Error('Building is an abstract class and cannot be instantiated directly');
+    }
     this._sqft = sqft;
   }
 
