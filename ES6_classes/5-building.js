2ac2abe285ab699bb2f4ed9c61cf2c7cbec12c2f
BoostedT <tyler.whitchurch@atlasschool.com>
2025-03-10 13:05:07 -0500

    removed extra code

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -7,8 +7,4 @@ export default class Building {
   get sqft() {
     return this._sqft;
   }
-
-  evacuationWarningMessage() {
-    throw new Error("Class extending Building must override evacuationWarningMessage");
-  }
 }
