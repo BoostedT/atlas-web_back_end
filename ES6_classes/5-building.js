97a2a98307719cfcd18376600f249536bee5961c
BoostedT <tyler.whitchurch@atlasschool.com>
2025-03-10 12:39:37 -0500

    im fixing the issue

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,8 +1,8 @@
 export default class Building {
   constructor(sqft) {
     if (new.target === Building) {
-    throw new Error("Building is an abstract class and cannot be instantiated directly");
-  }
+      throw new Error("Building is an abstract class and cannot be instantiated directly");
+    }
 
     if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
       throw new Error("Class extending Building must override evacuationWarningMessage");
