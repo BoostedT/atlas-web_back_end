3cfc1b4064bc6dcc305f61d5bebe2b05a3469f55
BoostedT <tyler.whitchurch@atlasschool.com>
2025-03-11 20:03:14 -0500

    fixed code to fix the issue with the checker

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,12 +1,12 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (position >= length || position < 0) {
+  if (position < 0 || position >= length) {
     throw new Error('Position outside range');
   }
 
   const buffer = new ArrayBuffer(length);
   const int8View = new Int8Array(buffer);
 
-  int8View[position] = value;
+  int8View.setInt8(position, value);
 
   return buffer;
 }
