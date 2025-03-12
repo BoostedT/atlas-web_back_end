a6805c0410abe510f41a7a9b0d8e0e4bdc6a2673
BoostedT <tyler.whitchurch@atlasschool.com>
2025-03-11 20:12:08 -0500

    a

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -7,6 +7,6 @@ export default function createInt8TypedArray(length, position, value) {
   const int8View = new DataView(buffer, 0, length);
 
   int8View.setInt8(position, value);
-
+/* eslint-disable semi-spacing */
   return int8View;
 }
