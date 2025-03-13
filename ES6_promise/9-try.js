0a648df0a0fed157a3dd159ee07745a2d818b0e4
BoostedT <tyler.whitchurch@atlasschool.com>
2025-03-12 23:41:53 -0500

    123

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -7,6 +7,6 @@ export default function guardrail(mathFunction) {
     queue.push(`Error: ${error.message}`);
   }
 
-  queue.push("Guardrail was processed");
+  queue.push('Guardrail was processed');
   return queue;
 }
