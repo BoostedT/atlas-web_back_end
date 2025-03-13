e1901a920172c8b658654b2f3ab26c6ee68c03b2
BoostedT <tyler.whitchurch@atlasschool.com>
2025-03-12 20:11:27 -0500

    retrying for checker

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,10 +1,9 @@
 export default function groceriesList() {
-  const groceries = {
-    Apples: 10,
-    Tomatoes: 10,
-    Pasta: 1,
-    Rice: 1,
-    Banana: 5
-  };
-  return groceries;
+  return new Map([
+    ['Banana', 10],
+    ['Tomato', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
 }
