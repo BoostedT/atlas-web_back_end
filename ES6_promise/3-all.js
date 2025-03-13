b0a24e82c44bf7d8a8a7b78f94029225901c6319
BoostedT <tyler.whitchurch@atlasschool.com>
2025-03-12 22:32:41 -0500

    merged import

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,6 +1,5 @@
 /* eslint-disable import/extensions */
-import { uploadPhoto } from './utils.js';
-import { createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
