aa120278db5564bca5c6f269161e9aa52f80633c
BoostedT <tyler.whitchurch@atlasschool.com>
2025-03-12 20:25:05 -0500

    lint fixes

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,13 +1,13 @@
 export default function updateUniqItems(groceriesMap) {
   if (!(groceriesMap instanceof Map)) {
-    throw new Error("Cannot process");
-}
-
-for (const [key, value] of groceriesMap) {
-  if (value === 1) {
-    groceriesMap.set(key, 100);
+    throw new Error('Cannot process');
   }
-}
 
-return groceriesMap;
+  for (const [key, value] of groceriesMap) {
+    if (value === 1) {
+      groceriesMap.set(key, 100);
+    }
+  }
+
+  return groceriesMap;
 }
