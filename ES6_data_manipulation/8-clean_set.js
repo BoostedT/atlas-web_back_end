fc710b6bd5600384d8de8c1d16bd3839805f84df
BoostedT <tyler.whitchurch@atlasschool.com>
2025-03-12 19:49:00 -0500

    lint update

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   if (!startString || typeof startString !== 'string') return '';
-
+/* eslint-disable arrow-parens */
   return Array.from(set)
     .filter(value => typeof value === 'string' && value.startsWith(startString))
     .map(value => value.slice(startString.length))
