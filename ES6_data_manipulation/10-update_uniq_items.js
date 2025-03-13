c16307ce9892214470d3dd91b409121fb5f79427
BoostedT <tyler.whitchurch@atlasschool.com>
2025-03-12 20:21:49 -0500

    testing this out

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,12 @@
+export default function updateUniqItems(groceriesMap) {
+  if (!(groceriesMap instanceof Map))
+    throw new Error("Cannot process");
+}
+
+for (const [key, value] of groceriesMap) {
+  if (value === 0) {
+    groceriesMap.set(key, 100);
+  }
+
+return groceriesMap;
+}
