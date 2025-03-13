da4081741a32ead300eeac49b9789a18fae9f29a
BoostedT <tyler.whitchurch@atlasschool.com>
2025-03-12 22:14:38 -0500

    lint

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,9 +1,11 @@
+  /* eslint-disable object-curly-newline,brace-style */
 export default function getFullResponseAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
       resolve({
         status: 200,
-        body: 'Success' });
+        body: 'Success',
+      });
     }
     else {
       reject(new Error('The fake API is not working currently'));
