db91eb74a21f3f22cf6c698f8d1b5ab35a792c26
BoostedT <tyler.whitchurch@atlasschool.com>
2025-03-12 23:53:36 -0500

    last try

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,14 +2,14 @@
 import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((results) =>
       results.map((result) => ({
         status: result.status,
         value: result.status === 'fulfilled'
           ? result.value
-          : (result.reason instanceof Error ? result.reason.message : result.reason), // Ensure error message is a string
+          : (result.reason instanceof Error ? result.reason.message : String(result.reason)),
       }))
     );
 }
