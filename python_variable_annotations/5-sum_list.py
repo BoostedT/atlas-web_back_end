afd446e5796766ad3a8c599cf1870799d7464159
BoostedT <tyler.whitchurch@atlasschool.com>
2025-05-08 16:50:51 -0500

    capitalized L on list

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -4,6 +4,6 @@
 from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Return the sum of all elements in the input list."""
     return sum(input_list)
