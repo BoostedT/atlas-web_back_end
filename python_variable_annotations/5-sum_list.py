4b2fa5c8ff77584a72447e7b2de6fa6bc6221098
BoostedT <tyler.whitchurch@atlasschool.com>
2025-05-08 16:48:30 -0500

    i think this will get the sum of the list as a float

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" task 5 sum_list.py"""
+
+
+def sum_list(input_list: list[float]) -> float:
+  """Return the sum of all elements in the input list."""
+  return sum(input_list)
