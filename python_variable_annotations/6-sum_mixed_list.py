8334aaa6b266d1697d887bf57b50a4274efa5251
BoostedT <tyler.whitchurch@atlasschool.com>
2025-05-09 17:56:23 -0500

    returns the sum as integers and floats

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""task 6 - sum_mixed_list"""
+
+
+def sum_mixed_list(mxd_lst: list[float | int]) -> float:
+    """Returns the sum of a mixed list of integers and floats"""
+    return sum(mxd_lst)
