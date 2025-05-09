53fb5ad775cfef3c93a46f1c4f1ea9348fa48fca
BoostedT <tyler.whitchurch@atlasschool.com>
2025-05-09 18:01:17 -0500

    made changes to code

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """task 6 - sum_mixed_list"""
 
+from typing import Union
 
-def sum_mixed_list(mxd_lst: list[float | int]) -> float:
+
+def sum_mixed_list(mxd_lst: list[Union[float | int]]) -> float:
     """Returns the sum of a mixed list of integers and floats"""
-    return sum(mxd_lst)
+    return float(sum(mxd_lst))
