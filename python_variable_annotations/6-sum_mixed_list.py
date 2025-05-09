9247f9351c4a7d0a113214860102e75e4bfba6ee
BoostedT <tyler.whitchurch@atlasschool.com>
2025-05-09 18:02:22 -0500

    fixed spelling on list to L and added import for List

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """task 6 - sum_mixed_list"""
 
-from typing import Union
+from typing import List,Union
 
 
-def sum_mixed_list(mxd_lst: list[Union[float | int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[float | int]]) -> float:
     """Returns the sum of a mixed list of integers and floats"""
     return float(sum(mxd_lst))
