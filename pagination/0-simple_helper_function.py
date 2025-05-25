4e6ab82c2e8adb082ac725c6110c957b04ae0a4d
BoostedT <tyler.whitchurch@atlasschool.com>
2025-05-25 16:28:20 -0500

    pep8

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 """task 0 0-simple_helper_function"""
 
+
 def index_range(page: int, page_size: int) -> tuple[int, int]:
     """
     Return a tuple of start and end index for pagination.
-    
+
     Args:
         page (int): The current page number (1-indexed).
         page_size (int): The number of items per page.
-    
+
     Returns:
         tuple[int, int]: A tuple containing the start and end index.
     """
