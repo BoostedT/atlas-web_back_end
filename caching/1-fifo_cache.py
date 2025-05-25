706c5c754217c992f39442890a9fa9563d189d9a
BoostedT <tyler.whitchurch@atlasschool.com>
2025-05-25 13:03:46 -0500

    added fifo

---
+++ caching/1-fifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""task 1 1-fifo_cache.py"""
+
+from BaseCaching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """FIFOCache inherits from BaseCaching and is a caching system"""
+
+    def __init__(self):
+        """Initialize the cache"""
+        super().__init__()
+        self.cache_order = []
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key and item:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                # Remove the first item in the cache
+                oldest_key = self.cache_order.pop(0)
+                del self.cache_data[oldest_key]
+                print("DISCARD: {}".format(oldest_key))
+            self.cache_data[key] = item
+            self.cache_order.append(key)
+
+    def get(self, key):
+        """Get an item by key"""
+        return self.cache_data.get(key, None)
