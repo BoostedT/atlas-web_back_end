2f1efed39840005ea348d7022d6dd574865e4333
BoostedT <tyler.whitchurch@atlasschool.com>
2025-05-26 11:00:36 -0500

    not sure if it works cant test it locally

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -14,3 +14,17 @@ def hash_password(password: str) -> bytes:
         bytes: The salted, hashed password.
     """
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validates a password against a hashed password.
+
+    Args:
+        hashed_password (bytes): The hashed password to validate against.
+        password (str): The plain-text password to check.
+
+    Returns:
+        bool: True if the password matches the hash, False otherwise.
+    """
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
