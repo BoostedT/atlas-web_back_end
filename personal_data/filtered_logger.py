029f765353ca267a4133e3215c6f85ee63bd3e41
BoostedT <tyler.whitchurch@atlasschool.com>
2025-05-26 10:24:14 -0500

    Filtered logger task 0 and 1

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import re
+from typing import List
+import logging
+
+
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator: str
+) -> str:
+    return re.sub(
+        rf'({"|".join(fields)})=.*?{separator}',
+        lambda m: f"{m.group(1)}={redaction}{separator}",
+        message + separator,
+    )[: -len(separator)]
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        original = super().format(record)
+        return filter_datum(
+          self.fields, self.REDACTION, original, self.SEPARATOR)
