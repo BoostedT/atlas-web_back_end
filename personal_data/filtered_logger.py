2299ff8f5e7bfe5d6103f0667d8fb0a25db72111
BoostedT <tyler.whitchurch@atlasschool.com>
2025-05-26 10:27:07 -0500

    more doc

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -8,12 +8,13 @@ import logging
 def filter_datum(
     fields: List[str], redaction: str, message: str, separator: str
 ) -> str:
+    """Redacts specified fields in a log message."""
     return re.sub(
         rf'({"|".join(fields)})=.*?{separator}',
         lambda m: f"{m.group(1)}={redaction}{separator}",
         message + separator,
     )[: -len(separator)]
-
+    
 
 class RedactingFormatter(logging.Formatter):
     """Redacting Formatter class"""
