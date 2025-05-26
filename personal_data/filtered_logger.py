b6c6ffc077c00a7bf1be4bd74ebdd2986745a231
BoostedT <tyler.whitchurch@atlasschool.com>
2025-05-26 10:36:03 -0500

    tasks 2 and 3 not sure about 3 yet will check

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,6 +3,11 @@
 import re
 from typing import List
 import logging
+import os
+import mysql.connector
+from mysql.connector.connection import MySQLConnection
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(
@@ -32,3 +37,31 @@ class RedactingFormatter(logging.Formatter):
         original = super().format(record)
         return filter_datum(
           self.fields, self.REDACTION, original, self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """
+    Creates and returns a logger named 'user_data'
+    with custom redacting formatter.
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    logger.addHandler(stream_handler)
+
+    return logger
+
+
+def get_db() -> MySQLConnection:
+    """
+    Returns a MySQL connector connection using credentials from environment variables.
+    """
+    return mysql.connector.connect(
+        user=os.environ.get("PERSONAL_DATA_DB_USERNAME", "root"),
+        password=os.environ.get("PERSONAL_DATA_DB_PASSWORD", ""),
+        host=os.environ.get("PERSONAL_DATA_DB_HOST", "localhost"),
+        database=os.environ["PERSONAL_DATA_DB_NAME"]
+    )
