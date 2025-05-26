6e010608855907ce22dfc0617e20c96cedef07d2
BoostedT <tyler.whitchurch@atlasschool.com>
2025-05-26 10:36:41 -0500

    pycode

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -57,7 +57,9 @@ def get_logger() -> logging.Logger:
 
 def get_db() -> MySQLConnection:
     """
-    Returns a MySQL connector connection using credentials from environment variables.
+    Returns a MySQL connector 
+    connection using credentials 
+    from environment variables.
     """
     return mysql.connector.connect(
         user=os.environ.get("PERSONAL_DATA_DB_USERNAME", "root"),
