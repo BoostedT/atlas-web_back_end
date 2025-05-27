944ea62fd389de99ae6d01812a1032ddfa9e0489
BoostedT <tyler.whitchurch@atlasschool.com>
2025-05-27 18:05:28 -0500

    pep

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Flask App"""
+"""Flask App"""
 
 from flask import Flask, jsonify, request
 from auth import Auth
@@ -8,22 +8,22 @@ app = Flask(__name__)
 auth = Auth()
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def index():
     """Index route"""
     return jsonify({"message": "Bienvenue"})
 
-@app.route('/users', methods=['POST'])
+
+@app.route("/users", methods=["POST"])
 def users():
     """Users route"""
-    email = request.form.get('email')
-    password = request.form.get('password')
-    
+    email = request.form.get("email")
+    password = request.form.get("password")
     try:
-      auth.register_user(email, password)
-      return jsonify({"email": email, "message": "user created"}), 200
+        auth.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 200
     except Exception:
-      return jsonify({"message": "email already exists"}), 400
+        return jsonify({"message": "email already exists"}), 400
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
