a07206781a1fa8e66557d8947c73760bbbe8e0d7
BoostedT <tyler.whitchurch@atlasschool.com>
2025-05-26 20:20:57 -0500

    pep8

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -35,12 +35,14 @@ def forbidden(error) -> str:
     """Custom Forbidden handler"""
     return jsonify({"error": "Forbidden"}), 403
 
+
 @app.before_request
 def before_request() -> None:
     """Before request handler"""
     if auth is None:
         return
-    excluded_paths = ["/api/v1/status/", "/api/v1/unauthorized/", "/api/v1/forbidden/"]
+    excluded_paths = [
+      "/api/v1/status/", "/api/v1/unauthorized/", "/api/v1/forbidden/"]
     if not auth.require_auth(request.path, excluded_paths):
         return
     if not auth.authorization_header(request):
