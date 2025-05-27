f947aed0c8b2eff35e98f78a4ac95227b4d2241d
BoostedT <tyler.whitchurch@atlasschool.com>
2025-05-27 14:00:22 -0500

    indent fix

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -62,7 +62,7 @@ def before_request() -> None:
     if not auth.require_auth(request.path, excluded_paths):
         return
     if auth.authorization_header(request) is None \
-    and auth.session_cookie(request) is None:
+            and auth.session_cookie(request) is None:
         abort(401)
     user = auth.current_user(request)
     if user is None:
