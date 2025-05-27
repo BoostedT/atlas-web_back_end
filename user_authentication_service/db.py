14c9cc1fd216e80d6fd801a2a9f634c5493ac0c5
BoostedT <tyler.whitchurch@atlasschool.com>
2025-05-27 16:09:54 -0500

    finduserby

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python3
-"""DB module
-"""
+"""DB module"""
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base
 from user import User
 
 
 class DB:
-    """DB class
-    """
+    """DB class"""
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
-        """
+        """Initialize a new DB instance"""
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
@@ -24,17 +23,29 @@ class DB:
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
-        """
+        """Memoized session object"""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a new user to the database
-        """
+        """Add a new user to the database"""
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
         return new_user
+      
+      
+    def find_user_by(self, **kwargs) -> User:
+        """Find the first user that matches the given criteria."""
+        if not kwargs:
+            raise InvalidRequestError("No filter arguments provided.")
+
+        try:
+            user = self._session.query(User).filter_by(**kwargs).one()
+            return user
+        except NoResultFound:
+            raise
+        except InvalidRequestError:
+            raise
