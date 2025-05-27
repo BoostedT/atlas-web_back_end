4e1ac7ec4f542c999dca82a1f61403aaf3a0ea0e
BoostedT <tyler.whitchurch@atlasschool.com>
2025-05-27 16:37:11 -0500

    hash password

---
+++ user_authentication_service/auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""authentication"""
+
+import bcrypt
+
+def _hash_password(password: str) -> bytes:
+    """
+    Hashes a password using bcrypt.
+    """
+    
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
