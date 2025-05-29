653f0811564e8a6266710f0dbff172a660c9864f
BoostedT <tyler.whitchurch@atlasschool.com>
2025-05-28 23:17:57 -0500

    pep

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -86,12 +86,16 @@ class Auth:
             return token
         except NoResultFound:
             raise ValueError("No user found with this email")
-          
+
     def update_password(self, reset_token: str, password: str) -> None:
         """Update the user's password using the reset token."""
         try:
             user = self._db.find_user_by(reset_token=reset_token)
             hashed_password = _hash_password(password)
-            self._db.update_user(user.id, hashed_password=hashed_password.decode("utf-8"), reset_token=None)
+            self._db.update_user(
+                user.id,
+                hashed_password=hashed_password.decode("utf-8"),
+                reset_token=None,
+            )
         except NoResultFound:
             raise ValueError("Invalid reset token")
