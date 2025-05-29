a50765bf17548397c6411006ebf98eee0e6fc873
BoostedT <tyler.whitchurch@atlasschool.com>
2025-05-28 23:15:10 -0500

    PEP

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -76,13 +76,13 @@ class Auth:
             self._db.update_user(user_id, session_id=None)
         except NoResultFound:
             return None
-          
+
     def get_reset_password_token(self, email: str) -> str:
-      """Generate a reset password token for the user."""
-      try:
+        """Generate a reset password token for the user."""
+        try:
             user = self._db.find_user_by(email=email)
             token = _generate_uuid()
             self._db.update_user(user.id, reset_token=token)
             return token
-      except NoResultFound:
+        except NoResultFound:
             raise ValueError("No user found with this email")
