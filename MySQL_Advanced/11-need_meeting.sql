2d1afe5a5840a61592606ffcd6d13d4d272bf38c
BoostedT <tyler.whitchurch@atlasschool.com>
2025-06-17 18:17:37 -0500

    11 and onto nosql

---
+++ MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,10 @@
+-- Create a view named `need_meeting` that selects the names of students
+
+CREATE VIEW need_meeting AS
+SELECT name
+FROM students
+WHERE score < 80
+  AND (
+    last_meeting IS NULL
+    OR last_meeting < CURDATE(), INTERVAL 1 MONTH
+  );
